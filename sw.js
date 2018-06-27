a24994c0e0fb525098910d6c9106addbc4e8b9fe
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-27 23:04:43 +0100

    Modification to sw

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
  event.waitUntil(
    caches.open(staticCacheName).then(function(cache) {
      return cache.addAll([
-       '/',
+       '/skeleton',
        '/index.html',
        '/index.html?homescreen=1',
        '/?homescreen=1',
