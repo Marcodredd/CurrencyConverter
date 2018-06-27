8e2b61d9138db2178d30e38ce58d1cb090c2584d
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-27 22:47:53 +0100

    Another update to service worker

---
+++ sw.js
@@ -0,0 +1,23 @@
+let staticCacheName = 'currency-converter';
+
+self.addEventListener('install', function(event) {
+ event.waitUntil(
+   caches.open(staticCacheName).then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/curr.css',
+       '/curr.js',
+       '/calc.js'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+
+console.log(event.request.url);
+
+});
