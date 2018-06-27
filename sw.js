ebd98ca64333a792f0f03854656b63474a7b7240
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-27 23:17:12 +0100

    Modification to sw code

--- sw.js
+++ sw.js
@@ -4,13 +4,12 @@ self.addEventListener('install', function(event) {
  event.waitUntil(
    caches.open(staticCacheName).then(function(cache) {
      return cache.addAll([
-       '/skeleton',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/curr.css',
-       '/curr.js',
-       '/calc.js'
+       'index.html',
+       'index.html?homescreen=1',
+       '?homescreen=1',
+       'curr.css',
+       'curr.js',
+       'calc.js'
      ]);
    })
  );
