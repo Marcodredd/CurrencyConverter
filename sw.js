5390605bf21d60c24a053d65090bf7f14dd681c6
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-27 23:37:27 +0100

    Updated sw cache

--- sw.js
+++ sw.js
@@ -9,7 +9,9 @@ self.addEventListener('install', function(event) {
        '?homescreen=1',
        'curr.css',
        'curr.js',
-       'calc.js'
+       'calc.js',
+       'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+       'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'
      ]);
    })
  );
