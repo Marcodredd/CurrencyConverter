182724854ab42afde64d6f007f4398a1a049865e
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-27 23:27:59 +0100

    Added offline-first request

--- sw.js
+++ sw.js
@@ -16,7 +16,9 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-
-console.log(event.request.url);
-
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
 });
