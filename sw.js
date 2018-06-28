9ed97e8729a9a942b60642eb44bbee75b8fbf5d7
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-28 08:27:23 +0100

    Added github gh-pages to cache file

--- sw.js
+++ sw.js
@@ -9,7 +9,8 @@ self.addEventListener('install', function(event) {
        'curr.js',
        'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
        'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css',
-       'https://free.currencyconverterapi.com/api/v5/countries'
+       'https://free.currencyconverterapi.com/api/v5/countries',
+       'https://marcodredd.github.io/CurrencyConverter/'
      ]);
    })
  );
