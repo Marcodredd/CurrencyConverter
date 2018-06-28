d8c3cb84c399bf2f865b05df3ec0c97d0a1db8ef
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-28 21:15:43 +0100

    Modification to dbPromise

--- calc.js
+++ calc.js
@@ -3,38 +3,12 @@
 if (!('indexedDB' in window)) {
 	console.log('This browser doesn\'t support IndexedDB');
 }
-
-
-const apiURL = `https://free.currencyconverterapi.com/api/v5/countries`;   
-let countriesCurrencies;
-const dbPromise = idb.open('countries-currencies', 0, upgradeDB => {
-  // Note: we don't use 'break' in this switch statement,
-  // the fall-through behaviour is what we want.
-  switch (upgradeDB.oldVersion) {
-    case 0:
-      upgradeDB.createObjectStore('objs', {keyPath: 'id'});
-  }
+let dbPromise = idb.open('currency-converter', 2, upgradeDb => {
+	switch(upgradeDb.oldVersion) {
+		case 0:
+		upgradeDb.createObjectStore('currency', {keypath: 'id'});
+	}
 });
-fetch(apiURL)
-  .then(function(response) {
-  return response.json();
-})
-  .then(function(currencies) {
-  dbPromise.then(db => {
-    if(!db) return;
-    countriesCurrencies = [currencies.results];
-    const tx = db.transaction('objs', 'readwrite');
-    const store = tx.objectStore('objs');
-    let i = 0;
-    countriesCurrencies.forEach(function(currency) {
-      for (let value in currency) {
-        store.put(currency[value]);
-      }
-    });
-    return tx.complete;
-  });
-});
-
 
 fetch('https://free.currencyconverterapi.com/api/v5/countries')
   .then(
