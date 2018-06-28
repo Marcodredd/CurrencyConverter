6c9ea7c736beb81f15d1655e839a4b357808aee2
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-28 09:01:36 +0100

    minor update to sw

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ self.addEventListener('install', function(event) {
        'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
        'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css',
        'https://free.currencyconverterapi.com/api/v5/countries',
-       'https://marcodredd.github.io/CurrencyConverter'
+       'https://marcodredd.github.io/CurrencyConverter/'
      ]);
    })
  );
