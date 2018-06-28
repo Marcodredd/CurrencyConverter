620ba4b1c57e09f9128d8e5a9ec5444635df0093
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-28 08:42:04 +0100

    Making cache only request update

--- sw.js
+++ sw.js
@@ -17,9 +17,6 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
+	event.respondWith(fetch(event.request));
 });
+	
