c9dbbbcde5e29f34413c7ada9a6af87e0b4f51b4
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-06-30 13:27:53 +0100

    Updated sw

--- sw.js
+++ sw.js
@@ -24,14 +24,3 @@ self.addEventListener('fetch', function(event) {
 	}());
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-       );
-    })
-   );
-});
