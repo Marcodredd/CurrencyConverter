d74aa72b03e145ef904d44b4b9bab1814b35d6d3
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-07-01 15:50:55 +0100

    Updated sw fetch code

--- sw.js
+++ sw.js
@@ -17,10 +17,23 @@ self.addEventListener('install', function(event) {
  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request)),
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+        );
+    })
     );
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+    );
+});
